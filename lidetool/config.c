891b2e5a0d2a115771b7ce7648bdcee67fb894d5
Matt Harlum <matt@harlum.net>
2023-11-22 21:47:59 +0000

    Add lidetool

---
+++ lidetool/config.c
@@ -0,0 +1,83 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/* This file is part of lidetool
+ * Copyright (C) 2023 Matthew Harlum <matt@harlum.net>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+#include <stdbool.h>
+#include <proto/exec.h>
+#include <stdio.h>
+
+#include "main.h"
+#include "config.h"
+
+/** configure
+ *
+ * @brief Parse the command arguments and return the config
+ * @param argc Arg count
+ * @param argv Argument variables
+ * @returns Pointer to a Config struct or NULL on error
+*/
+struct Config* configure(int argc, char *argv[]) {
+
+  bool error = false;
+
+  struct Config *config;
+  config = (struct Config *)AllocMem(sizeof(struct Config),MEMF_CLEAR);
+  
+  if (config == NULL) return NULL;
+
+  config->Unit = -1;
+  config->Mode = -1;
+
+  for (int i=1; i<argc; i++) {
+    if (argv[i][0] == '-') {
+      switch(argv[i][1]) {
+        case 'm':
+          if (i+1 < argc) {
+            config->Mode = (*argv[i+1])-'0';
+            i++;
+          }
+          break;
+
+        case 'u':
+          if (i+1 < argc) {
+            config->Unit = (*argv[i+1])-'0';
+            i++;
+          }
+          break;
+
+      }
+    }
+  }
+
+  if (config->Unit == -1 || config->Mode == -1) {
+      error = true;
+  }
+
+  if (error) {
+    FreeMem(config,sizeof(struct Config));
+    return (NULL);
+  } else {
+    return (config);
+  }
+}
+
+/** usage
+ * @brief Print the usage information
+*/
+void usage() {
+    printf("\nUsage: lidetool -u <unit> -m <method>\n\n");
+}
