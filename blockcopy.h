20de9f3f72bf4735d7c1b3e470baf88987362bbb
Matt Harlum <matt@harlum.net>
2023-11-28 16:52:01 +0000

    Tweak Blockcopy

--- blockcopy.h
+++ blockcopy.h
@@ -8,62 +8,27 @@
  * Adapted from the open source at_apollo_device by Frédéric REQUIN
  * https://github.com/fredrequin/at_apollo_device
  * 
- * NOTE! source needs to be 48 bytes before the end of the data port!
+ * NOTE!
  * The 68000 does an extra memory access at the end of a movem instruction!
  * Source: https://github.com/prb28/m68k-instructions-documentation/blob/master/instructions/movem.md
  * 
- * With the src of end-48 the error reg will be harmlessly read instead.
+ * With the src of end-52 the error reg will be harmlessly read instead.
  * 
  * @param source Pointer to drive data port
  * @param destination Pointer to source buffer
 */
 static inline void ata_read_long_movem (void *source, void *destination) {
-    source += (512 - 48);
 
-    asm volatile ("moveq  #48,d7\n\t"
-
-    "movem.l (%0),d0-d6/a1-a4/a6\n\t"
-    "movem.l d0-d6/a1-a4/a6,(%1)\n\t"
-    "add.l   d7,%1\n\t" // 1
-    
-    "movem.l (%0),d0-d6/a1-a4/a6\n\t"
-    "movem.l d0-d6/a1-a4/a6,(%1)\n\t"
-    "add.l   d7,%1\n\t" // 2
-    
-    "movem.l (%0),d0-d6/a1-a4/a6\n\t"
-    "movem.l d0-d6/a1-a4/a6,(%1)\n\t"
-    "add.l   d7,%1\n\t" // 3
-    
-    "movem.l (%0),d0-d6/a1-a4/a6\n\t"
-    "movem.l d0-d6/a1-a4/a6,(%1)\n\t"
-    "add.l   d7,%1\n\t" // 4
-
-    "movem.l (%0),d0-d6/a1-a4/a6\n\t"
-    "movem.l d0-d6/a1-a4/a6,(%1)\n\t"
-    "add.l   d7,%1\n\t" // 5
-    
-    "movem.l (%0),d0-d6/a1-a4/a6\n\t"
-    "movem.l d0-d6/a1-a4/a6,(%1)\n\t"
-    "add.l   d7,%1\n\t" // 6
-
-    "movem.l (%0),d0-d6/a1-a4/a6\n\t"
-    "movem.l d0-d6/a1-a4/a6,(%1)\n\t"                  
-    "add.l   d7,%1\n\t" // 7
-
-    "movem.l (%0),d0-d6/a1-a4/a6\n\t"
-    "movem.l d0-d6/a1-a4/a6,(%1)\n\t"                   
-    "add.l   d7,%1\n\t" // 8
-
-    "movem.l (%0),d0-d6/a1-a4/a6\n\t"
-    "movem.l d0-d6/a1-a4/a6,(%1)\n\t"                   
-    "add.l   d7,%1\n\t" // 9
-
-    "movem.l (%0),d0-d6/a1-a4/a6\n\t"
-    "movem.l d0-d6/a1-a4/a6,(%1)\n\t"
-    "add.l   d7,%1\n\t" // 10
-
-    "movem.l 16(%0),d0-d6/a1\n\t"
-    "movem.l d0-d6/a1,(%1)\n\t"
+    asm volatile (
+    "lea.l   460(%0),%0                 \n\t"
+    "offset = 0                         \n\t"
+    ".rep 9                             \n\t"
+    "movem.l (%0),d0-d7/a1-a4/a6        \n\t"
+    "movem.l d0-d7/a1-a4/a6,offset(%1)  \n\t"
+    "offset = offset + 52               \n\t"
+    ".endr                              \n\t"
+    "movem.l 8(%0),d0-d7/a1-a3          \n\t"
+    "movem.l d0-d7/a1-a3,offset(%1)     \n\t"
     :
     :"a" (source),"a" (destination)
     :"a1","a2","a3","a4","a6","d0","d1","d2","d3","d4","d5","d6","d7"
@@ -81,41 +46,14 @@ static inline void ata_read_long_movem (void *source, void *destination) {
  * @param destination Pointer to drive data port
 */
 static inline void ata_write_long_movem (void *source, void *destination) {
-    destination += (512 - 48);
 
     asm volatile (
-    "movem.l (%0)+,d0-d6/a1-a4/a6\n\t"
-    "movem.l d0-d6/a1-a4/a6,(%1)\n\t"
-    
-    "movem.l (%0)+,d0-d6/a1-a4/a6\n\t"
-    "movem.l d0-d6/a1-a4/a6,(%1)\n\t"
-    
-    "movem.l (%0)+,d0-d6/a1-a4/a6\n\t"
-    "movem.l d0-d6/a1-a4/a6,(%1)\n\t"
-    
-    "movem.l (%0)+,d0-d6/a1-a4/a6\n\t"
-    "movem.l d0-d6/a1-a4/a6,(%1)\n\t"
-
-    "movem.l (%0)+,d0-d6/a1-a4/a6\n\t"
-    "movem.l d0-d6/a1-a4/a6,(%1)\n\t"
-    
-    "movem.l (%0)+,d0-d6/a1-a4/a6\n\t"
-    "movem.l d0-d6/a1-a4/a6,(%1)\n\t"
-
-    "movem.l (%0)+,d0-d6/a1-a4/a6\n\t"
-    "movem.l d0-d6/a1-a4/a6,(%1)\n\t"                  
-
-    "movem.l (%0)+,d0-d6/a1-a4/a6\n\t"
-    "movem.l d0-d6/a1-a4/a6,(%1)\n\t"                   
-
-    "movem.l (%0)+,d0-d6/a1-a4/a6\n\t"
-    "movem.l d0-d6/a1-a4/a6,(%1)\n\t"                   
-
-    "movem.l (%0)+,d0-d6/a1-a4/a6\n\t"
-    "movem.l d0-d6/a1-a4/a6,(%1)\n\t"
-
-    "movem.l (%0)+,d0-d6/a1\n\t"
-    "movem.l d0-d6/a1,(%1)\n\t"
+    ".rep 9                       \n\t"
+    "movem.l (%0)+,d0-d7/a1-a4/a6 \n\t"
+    "movem.l d0-d7/a1-a4/a6,(%1)  \n\t"
+    ".endr                        \n\t"
+    "movem.l (%0)+,d0-d7/a1-a3    \n\t"
+    "movem.l d0-d7/a1-a3,(%1)     \n\t"
     :
     :"a" (source),"a" (destination)
     :"a1","a2","a3","a4","a6","d0","d1","d2","d3","d4","d5","d6","d7"
@@ -125,12 +63,12 @@ static inline void ata_write_long_movem (void *source, void *destination) {
 /**
  * ata_read_long_move
  * 
- * Read a sector using move - faster than movem on 68030+
+ * Read a sector using move - faster than movem on 68020+
  * 
 */
 static inline void ata_read_long_move (void *source, void *destination) {
     asm volatile (
-        "move.l #3,d0          \n\t"
+        "move.l #3,d0           \n\t"
         ".l1:                   \n\t"
         ".rept  32              \n\t"
         "move.l (%0),(%1)+      \n\t"
@@ -145,7 +83,7 @@ static inline void ata_read_long_move (void *source, void *destination) {
 /**
  * ata_write_long_move
  * 
- * Write a sector using move - faster than movem on 68030+
+ * Write a sector using move - faster than movem on 68020+
  * 
 */
 static inline void ata_write_long_move (void *source, void *destination) {
